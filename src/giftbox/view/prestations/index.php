e20edca432d69fc5702b0e9f936a603d7e777532
TB <t.bourrely@openmailbox.org>
2017-01-02 11:57:53 +0100

    ajout ordre liste prestations

--- src/giftbox/view/prestations/index.php
+++ src/giftbox/view/prestations/index.php
@@ -1,8 +1,9 @@
 <?php $title = 'Prestations'; ?>
+<p><a href="<?php echo $url; ?>/asc">croissant</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo $url; ?>/desc">decroissant</a></p>
 <?php foreach ($prestations as $prestation => $p):
     $nom = \giftbox\models\Categorie::find($p->cat_id)->nom;
 ?>
-    <p><a href="<?php echo $url; ?>/asc">croissant</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo $url; ?>/desc">decroissant</a></p>
+
     <h2>Prestation : <?php echo $nom; ?></h2>
     <p><img class='prestaImg' src="<?php echo BASE_URL; ?>/web/img/<?php echo $p->img; ?>"></p>
     <h3>Categorie : <a href="<?php echo BASE_URL; ?>/categories/<?php echo $p->cat_id; ?>/asc"><?php echo $nom; ?></a></h3>
