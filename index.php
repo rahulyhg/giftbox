083dfd22b9fbb4b851372ec454647156caf69793
WILMOUTH Steven <steven.wilmouth1@etu.univ-lorraine.fr>
2017-01-03 17:28:50 +0100

    Vues

--- index.php
+++ index.php
@@ -24,35 +24,51 @@ $app->get('/',function(){
 });
 
 $app->get('/prestations/all/:order', function($order){
-    $controller = new \giftbox\controller\PrestationsController('index', $order);
+    if ($order == "desc"){
+        $liste = \giftbox\models\Prestation::all()->sortByDesc('prix');
+    }else{
+        $liste = \giftbox\models\Prestation::all()->sortBy('prix');
+    }
+    $vue = new \giftbox\view\PrestaView($liste->toArray());
+    $html = new \giftbox\view\htmlView($vue->render(1));
+    $html->render();
 });
 
 $app->get('/prestations/:id', function($id){
-    $controller = new \giftbox\controller\PrestationsController('view', $id);
+   $prestation = \giftbox\models\Prestation::find($id);
+    $vue = new \giftbox\view\PrestaView([$prestation]);
+    $html = new \giftbox\view\htmlView($vue->render(2));
+    $html->render();
 });
 
 $app->get('/categories/', function(){
-    $controller = new \giftbox\controller\CategoriesController('index');
+    $categories = \giftbox\models\Categorie::all();
+    $vue = new \giftbox\view\CatView($categories);
+    $html = new \giftbox\view\htmlView($vue->render(1));
+    $html->render();
 });
 
 $app->get('/categories/:categorie/:order', function($categorie, $order){
-    $controller = new \giftbox\controller\CategoriesController('view', ['id' => $categorie, 'order' => $order]);
+    $cat = \giftbox\models\Categorie::find($categorie);
+    $vue = new \giftbox\view\CatView([$cat], $order);
+    $html = new \giftbox\view\htmlView($vue->render(2));
+    $html->render();
 });
 
 $app->get('/prestation/add/:id', function($id) {
-    $controller = new \giftbox\controller\PrestationsController('add', $id);
+
 });
 
 $app->get('/panier', function() {
-    $controller = new \giftbox\controller\PanierController('index');
+
 });
 
 $app->get('/panier/delete/:id', function($id) {
-    $controller = new \giftbox\controller\PanierController('delete', $id);
+
 });
 
 $app->get('/panier/save', function() {
-    $controller = new \giftbox\controller\PanierController('save');
+
 });
 
 $app->run();
--- src/giftbox/view/categories/index.php
+++
@@ -1,5 +0,0 @@
-<?php $title = 'Catégories'; ?>
-<?php foreach ($categories as $category => $c): ?>
-    <h2><?php echo $c->nom; ?></h2>
-    <p><a href="<?php echo BASE_URL; ?>/categories/<?php echo $c->id; ?>/asc">voir les prestations</a></p>
-<?php endforeach; ?>
