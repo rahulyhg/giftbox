67bd1d2c4f42a492b6537865e1c4e11fb0addcc7
TB <t.bourrely@openmailbox.org>
2017-01-03 17:30:18 +0100

    controllers

---
+++ src/giftbox/controller/PanierController.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Steven
+ * Date: 30/12/2016
+ * Time: 23:30
+ */
+
+namespace giftbox\controller;
+
+
+use giftbox\models\Categorie;
+use giftbox\models\Prestation;
+
+class PanierController extends BaseController
+{
+
+    protected $name = 'panier';
+
+    public function index() {
+        if (isset($_SESSION['panier'])) {
+            $this->set('panier', $_SESSION['panier']);
+        } else {
+            $_SESSION['flash'] = array(
+                'message' => 'Panier vide',
+                'type' => 'info'
+            );
+            $this->set('panier', null);
+        }
+    }
+
+    public function delete($id) {
+        $prestation = Prestation::where('id', '=', $id)->first();
+        if (isset($_SESSION['panier']['article'][$prestation->nom])) {
+            $_SESSION['panier']['qua'] = ($_SESSION['panier']['qua'] - 1);
+            if ($_SESSION['panier']['article'][$prestation->nom]['qua'] == 1) {
+                unset($_SESSION['panier']['article'][$prestation->nom]);
+            } else {
+                $_SESSION['panier']['article'][$prestation->nom]['qua'] = ($_SESSION['panier']['article'][$prestation->nom]['qua'] - 1);
+                $_SESSION['panier']['article'][$prestation->nom]['prix'] = ($_SESSION['panier']['article'][$prestation->nom]['prix'] - $prestation->prix);
+            }
+            $_SESSION['flash'] = array(
+                'message' => 'Prestation supprimée du panier',
+                'type' => 'success'
+            );
+            $this->redirect('/panier');
+        } else {
+            $this->redirect('/panier');
+        }
+    }
+
+    public function save() {
+        if (isset($_SESSION['panier'])) {
+            $categories = array();
+            foreach ($_SESSION['panier']['article'] as $article => $a) {
+                $cat_id = Prestation::where('id', '=', $a['id'])->first()->cat_id;
+                $cat = Categorie::where('id', '=', $cat_id)->first();
+                if (!isset($categories[$cat['nom']])) {
+                    $categories[$cat['nom']] = 1;
+                } else {
+                    $categories[$cat['nom']] = ($categories[$cat['nom']] + 1);
+                }
+            }
+            var_dump( $categories);
+            $this->set('panier', $_SESSION['panier']);
+        } else {
+            $this->redirect(BASE_URL . '/');
+        }
+    }
+
+}
---
+++ src/giftbox/controller/PrestationsController.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Steven
+ * Date: 30/12/2016
+ * Time: 21:26
+ */
+
+namespace giftbox\controller;
+
+
+use giftbox\models\Prestation;
+
+class PrestationsController extends BaseController
+{
+
+    protected $name = 'prestations';
+
+    public function index($order) {
+        $prestations = \giftbox\models\Prestation::all()->sortBy('prix');
+        if ($order == "desc"){
+            $prestations = \giftbox\models\Prestation::all()->sortByDesc('prix');
+        }
+        $this->set('prestations', $prestations);
+        $url = BASE_URL . DIRECTORY_SEPARATOR . 'prestations' . DIRECTORY_SEPARATOR . 'all';
+        $this->set('url',$url);
+    }
+
+    public function view($id) {
+        $prestation = \giftbox\models\Prestation::find($id);
+        $this->set('prestation', $prestation);
+    }
+
+    public function add($id) {
+        $prestation = Prestation::where('id', '=', $id)->first();
+        if (!empty($prestation)) {
+            if (!isset($_SESSION['panier'])) {
+                $_SESSION['panier'] = array(
+                    'qua' => 1,
+                    'article' => array()
+                );
+                $_SESSION['panier']['article'][$prestation->nom] = array(
+                    'id' => $prestation->id,
+                    'qua' => 1,
+                    'prix' => $prestation->prix
+                );
+            } else {
+                $_SESSION['panier']['qua'] = ($_SESSION['panier']['qua'] + 1);
+                if (isset($_SESSION['panier']['article'][$prestation->nom])) {
+                    $_SESSION['panier']['article'][$prestation->nom]['qua'] = ($_SESSION['panier']['article'][$prestation->nom]['qua'] + 1);
+                    $_SESSION['panier']['article'][$prestation->nom]['prix'] = ($_SESSION['panier']['article'][$prestation->nom]['prix'] + $prestation->prix);
+                } else {
+                    $_SESSION['panier']['article'][$prestation->nom] = array(
+                        'id' => $prestation->id,
+                        'qua' => 1,
+                        'prix' => $prestation->prix
+                    );
+                }
+            }
+            $_SESSION['flash'] = array(
+                'message' => 'Préstation ajoutée au panier',
+                'type' => 'info'
+            );
+            if (strstr($_SERVER['HTTP_REFERER'], 'panier')) {
+                $this->redirect('/panier');
+            } else {
+                //$this->redirect('/prestations/all/asc');
+
+                $url = str_replace("http://localhost/projet_giftbox", "", $_SERVER['HTTP_REFERER']);
+
+                $this->redirect($url);
+            }
+        } else {
+            $this->redirect(BASE_URL . '/');
+        }
+    }
+
+}
