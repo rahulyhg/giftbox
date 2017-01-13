7d1b0f4b08c1f2e138caa8c65da630ff6da85e6b
WILMOUTH Steven <steven.wilmouth1@etu.univ-lorraine.fr>
2017-01-13 15:53:52 +0100

    Commencement integration bootstrap

--- src/giftbox/view/htmlCode.php
+++ src/giftbox/view/htmlCode.php
@@ -14,25 +14,97 @@ if ($flash != null) {
     $alertType = array_keys($flash)[0];
     $flashMessage = '<div class="alert alert-' . $alertType . '">' . $flash[$alertType] . '</div>';
 }
-echo '<!DOCTYPE html>
- <html>
-    <head>
-        <title>Accueil</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="' . $directory . 'web/css/style.css">
-    </head>
-    <body>
-        <p><a href="' . $directory . 'panier"><img src="' . $directory . 'web/img/cart.png" alt="Panier" width="24" class="cart">Article(s) : ' . (isset($_SESSION['panier']) ? $_SESSION['panier']['qua'] : '0') . '</a></p>
-        <nav>
-            <ul>
-                <li><a href="' . $directory . '">Accueil</a></li>
-                <li><a href="' . $directory . 'prestations/all/asc">prestations</a></li>
-                <li><a href="' . $directory . 'categories">categories</a></li>
-                <li><a href="' . $directory . 'administration">Administration</a>' . (isset($_SESSION['admin']) ? '&nbsp;|&nbsp;<a href="' . $directory . '/deconnexion">Se deconnecter</a>' : '') . '</li>
-            </ul>
-        </nav>
-        ' . $flashMessage . '
-        <content>' . $content . '</content>
-    </body>
+echo '
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="BOURRELY Thomas, WILMOUTH Steven">
+    <title>Off Canvas Template for Bootstrap</title>
+    <link href="' . $directory . 'web/css/bootstrap.min.css" rel="stylesheet">
+    <link href="' . $directory . 'web/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <link href="' . $directory . 'web/css/offcanvas.css" rel="stylesheet">
+    <script src="' . $directory . 'web/js/ie-emulation-modes-warning.js"></script>
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <nav class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="' . $directory . '">Accueil</a></li>
+            <li><a href="' . $directory . '/prestations/asc">Prestations</a></li>
+            <li><a href="' . $directory . '/categories">Catégories</a></li>
+            <li><a href="' . $directory . '/administration">Administration</a>' . (isset($_SESSION['admin']) ? '&nbsp;|&nbsp;<a href="' . $directory . '/deconnexion">Se deconnecter</a>' : '') . '</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="row row-offcanvas row-offcanvas-right">
+
+        <div class="col-xs-12 col-sm-9">
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+          </p>
+          <div class="row">
+            ' . $flashMessage . '
+            ' . $content . '
+            <div class="col-xs-6 col-lg-4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+          <div class="list-group">
+            <a href="#" class="list-group-item active">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+          </div>
+        </div>
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>&copy; 2017 IUT Nancy-Charlemagne, Département Informatique.</p>
+      </footer>
+
+    </div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="' . $directory . 'web/js/bootstrap.min.js"></script>
+    <script src="' . $directory . 'web/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="' . $directory . 'web/js/offcanvas.js"></script>
+  </body>
 </html>
 ';
